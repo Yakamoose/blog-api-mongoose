803709f78807dbf8f79a445349678b896fc804b7
Eric J <xericx.johnson@gmail.com>
2018-03-22 11:33:48 -0700

    uploading first try

---
+++ models.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const blogPostSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  author: {
+    firstName: String,
+    lastName: String
+  },
+  created: {type: Date, fefault: Date.now}
+});
+
+blogPostSchema.virtual('authorName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim()
+});
+
+blogPostSchema.methods.serialize = function() {
+  return {
+    title: this.title,
+    content: this.content,
+    author: this.authorName,
+    id: this._id,
+    created: this.created
+  };
+};
+
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+module.exports = {BlogPost};
